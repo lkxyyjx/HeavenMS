e7420cd73caf804b7898412255569f9cab4c6c2d
lkxyyjx <lkxyyjx@gmail.com>
2019-06-17 21:06:24 +0800

    linux enviroment

---
+++ compile.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo ant -Dplatforms.JDK_1.7.home=/opt/jdk1.7.0_80 compile
+sudo ant -Dplatforms.JDK_1.7.home=/opt/jdk1.7.0_80 jar
--- launch_server_linux.sh
+++ launch_server_linux.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-export CLASSPATH=".:dist/*" 
+export CLASSPATH=".:dist/*:cores/*"
+java -version
 java -Xmx2048m -Dwzpath=wz/ net.server.Server
